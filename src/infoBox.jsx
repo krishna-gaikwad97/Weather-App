341282fee490cdabe77ab29d7963aef19893b4f5
krishna gaikwad <gaikwadkrishnasunil@Outlook.com>
2025-12-10 21:53:28 +0530

    new data

--- src/infoBox.jsx
+++ src/infoBox.jsx
@@ -6,13 +6,15 @@ import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 export default function InfoBox({info}){
     const init_img="https://images.unsplash.com/photo-1653562969472-db4c9218278b?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fHRodW5kZXJzdHJvbWV8ZW58MHx8MHx8fDA%3D";
-    
+    const hot_img="https://media.istockphoto.com/id/1323823418/photo/low-angle-view-thermometer-on-blue-sky-with-sun-shining.webp?a=1&b=1&s=612x612&w=0&k=20&c=JfCdjP7brx9oUlLT6TIx9OTtEgvEGNpxDcDFqEz0Fo0=";
+    const cold_img="https://plus.unsplash.com/premium_photo-1669990950563-8b31d8ed237b?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fHdpbnRlcnxlbnwwfHwwfHx8MA%3D%3D";
+    const rainy_img="https://plus.unsplash.com/premium_photo-1670002347718-de1091111728?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cmFpbnklMjBkYXl8ZW58MHx8MHx8fDA%3D";
     return(
         <div className="infoBox">
              <Card sx={{ maxWidth: 345 }}>
       <CardMedia
         sx={{ height: 140 }}
-        image={init_img}
+        image={info.temp>303?hot_img:(info.temp<293?cold_img:(info.weatherType.includes("rain")?rainy_img:init_img))}
         title="green iguana"
       />
       <CardContent>
